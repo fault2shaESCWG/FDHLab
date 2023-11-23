1300601216ddb2cf580a27a77a01f02ca5ae1d7b
francescovisini <francesco.visini@ingv.it>
2023-11-23 17:40:50 +0100

    Update E_build_logistic_using_sitedimension.m

--- Regressions/E_build_logistic_using_sitedimension.m
+++ Regressions/E_build_logistic_using_sitedimension.m
@@ -2,6 +2,7 @@
 clear all
 
 %%   user defined inputs
+% calculate logistic for a given combination in "param_logistic"
 m = 6.5;
 x = 5:10:5000;
 site_dim = 500;
@@ -9,12 +10,13 @@ site_distance = 100;% meters from the PF
 HWFW = 'HW'; % HW = Hanging wall; FW = Footwall location of the site
 kin = 'Reverse';
 %kin = 'Normal';
+param_logistic = load(fullfile('TABLE_outputs',['parameters_logistic_multisizeC1_',char(kin),'.txt']));
+
 %%
 Pname = fullfile('TABLE_outputs',['P_montecarlo_SiteDim',num2str(site_dim),'_SiteDist',num2str(site_distance),'_',char(HWFW),'.txt']);
 P_montecarlo_table = readtable(Pname)
 P_montecarlo = P_montecarlo_table.Punif;
 %%
-param_logistic = load(fullfile('TABLE_outputs',['parameters_logistic_multisizeC1_',char(kin),'.txt']));
 i = find(param_logistic(1,:) ==site_dim);
 if strcmp(HWFW,'HW')==1
 y = param_logistic(2,i)+ param_logistic(3,i)*m + param_logistic(4,i).*x + param_logistic(5,i)*0;
